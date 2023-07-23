3555f1596a4f60d04b6bd9745255406030392400
Judeson Leo <judesonleo123@gmail.com>
2023-07-23 09:21:44 +0530

    first commit

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ const fs = require("fs");
 const salt = bcrypt.genSaltSync(10);
 const secret = "asdfe45we45w345wegw345werjktjwertkj";
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:3000", "https://Sparkle-spot-app.onrender.com"],
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
